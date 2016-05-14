7239cb4cf436ef89cef7c67a8981278e702967fc
Richard De Luca <richd711@gmail.com>
2016-05-13 23:31:24 -0400

    Update

--- src/com/deluca/util/Level.java
+++ src/com/deluca/util/Level.java
@@ -11,6 +11,9 @@ import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
@@ -22,23 +25,27 @@ public class Level {
 	MapLayer mapBaseLayer;
 	MapLayer mapOverlayLayer;
 	MapLayer mapCollisionLayer;
-	
+
 	SpriteBatch sb;
 	Texture texture;
 	public Sprite player;
 
-	// here
-	int mapx = 0;
-	int mapy = 0;
-
+	final int SCREENHEIGHT;
+	
+	int playerXMapCoord = 0;
+	int playerYMapCoord = 0;
+	int maxXCoord= -1;
+	int maxYCoord = -1;
 	int levelHeight = -1;
 	int levelWidth = -1;
-
+	float xCameraZoom = 1;
+	float yCameraZoom = 2;
+	int pixelWidth=-1;
+	int pixelHeight=-1;
 	
-
 	public Level(String levelName) {
 
-		tiledMap = new TmxMapLoader().load("level/"+levelName);
+		tiledMap = new TmxMapLoader().load("level/" + levelName);
 		MapProperties props = tiledMap.getProperties();
 		levelWidth = (Integer) props.get("width")
 				* (Integer) props.get("tilewidth");
@@ -46,27 +53,33 @@ public class Level {
 				* (Integer) props.get("tileheight");
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, levelWidth, levelHeight / 2);
+		camera.setToOrtho(false, levelWidth / xCameraZoom, levelHeight / yCameraZoom);
 		camera.update();
 
+		pixelWidth=(int) (Gdx.graphics.getWidth()/xCameraZoom);
+		pixelHeight=(int) (Gdx.graphics.getHeight()/yCameraZoom);
+		
 		// System.out.println(props.get());
 
-		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-
-		
-		//TODO: UN-HARDCODE
 		sb = new SpriteBatch();
+
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, sb);
+
+		// TODO: UN-HARDCODE
 		texture = new Texture(Gdx.files.internal("RookieBobby.png"));
 		player = new Sprite(texture);
-		player.setX(96);
-		player.setY(224);
-		
+		player.setX(0);
+		player.setY(0);
+		playerXMapCoord = (int) player.getX();
+		playerYMapCoord = (int) player.getY();
+		maxXCoord = levelWidth;
+		maxYCoord= (int) (levelHeight/yCameraZoom);
 		//
 		mapBaseLayer = tiledMap.getLayers().get(0);
 		mapOverlayLayer = tiledMap.getLayers().get(1);
-		mapCollisionLayer=tiledMap.getLayers().get(2);
-
-
+		mapCollisionLayer = tiledMap.getLayers().get(2);
+		mapCollisionLayer.getObjects();
+		SCREENHEIGHT=(int) (levelHeight/yCameraZoom);
 	}
 
 	public void renderbot() {
@@ -74,98 +87,154 @@ public class Level {
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
-		
-		tiledMapRenderer.setView(camera);
-		tiledMapRenderer.render(new int[1]);
+
 		sb.begin();
+
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.renderTileLayer((TiledMapTileLayer) mapBaseLayer);
 		player.draw(sb);
 		sb.end();
 	}
 
-	public void renderOverlay()
-	{
-		tiledMap.
-		tiledMapRenderer.render(new int[2]);		
+	public void renderOverlay() {
+		sb.begin();
+
+		tiledMapRenderer.renderTileLayer((TiledMapTileLayer) mapOverlayLayer);
+		sb.end();
 	}
 
 	public boolean keyUp(int keycode) {
+		MapProperties props = tiledMap.getProperties();
 
-		//sprite.getY()+((3*sprite.getHeight())*sprite.getScaleY())<Gdx.graphics.getHeight()
-
-		float characterEdgeBuffer=((3*player.getHeight())*player.getScaleY());
-		
-		switch (keycode)
+		// if()
 		{
-		case Input.Keys.W:
-			if(player.getY()+characterEdgeBuffer<Gdx.graphics.getHeight())
-			{
-				player.setY(player.getY() + 32);
+			// CELLS ARE: 20x20
+			// SCREEN IS: 1280x720
+			// "cell size": 64x75
+
+			// 75 height of cell
+
+			// Width is 20 tiles.
+			// Screen width is 1280
+			// viewportWidth=640.0
+			// RATIO OF WIDTH TO VIEWPORT IS: 2:1
+			// 64 pixel wide tiles
+			// 64 px/tile = 1280px/20 tiles
+			// pxPerTileWide= pixelWidth/numTiles
+			// 1.6px= x/y
+			//
+
+			int numTilesWide = (int) ((Gdx.graphics.getWidth() / camera.viewportWidth));
+			int screenTileWidth = Gdx.graphics.getWidth() / numTilesWide;
+			int numTilesHigh = (int) ((Integer) (tiledMap.getProperties()
+					.get("height")) / (Gdx.graphics.getHeight() / camera.viewportHeight));
+			int screenTileHigh = Gdx.graphics.getHeight() / numTilesHigh;
+
+			float msx = Gdx.input.getX();
+			float msy = (Gdx.graphics.getHeight() - Gdx.input.getY());
+
+			int cellX = (int) (msx / screenTileWidth);
+			int cellY = (int) (msy / screenTileHigh);
+			/*
+			 * System.out.println(props.get("tilewidth"));
+			 * System.out.println(props.get("tileheight"));
+			 * System.out.println("msx:"+msy+" msy:"+msy);
+			 * System.out.println("cellX:"+cellX+" cellY:"+cellY);
+			 */
+
+			Cell cell = ((TiledMapTileLayer) mapOverlayLayer).getCell(cellX,
+					cellY);
+			if (cell != null) {
+				TiledMapTile i = cell.getTile();
+				i.getId();
 			}
-			else
+			//System.out.println(cell != null);
+		}
+
+		// sprite.getY()+((3*sprite.getHeight())*sprite.getScaleY())<Gdx.graphics.getHeight()
+
+		float characterEdgeBuffer = ((3 * player.getHeight()) * player
+				.getScaleY());
+
+		switch (keycode) 
+		{
+
+		case Input.Keys.W:
+			if (playerYMapCoord + characterEdgeBuffer < pixelHeight) 
 			{
-				if (mapy < camera.viewportHeight) {
+				playerYMapCoord += 32;
+				player.setY(player.getY() + 32);
+				System.out.println("UP");
+			} 
+			else 
+			{
+				if (playerYMapCoord < camera.viewportHeight && maxYCoord<levelHeight) 
+				{
 					camera.translate(0, 32);
-					mapy += 32;
+					maxYCoord+=32;
+					playerYMapCoord-=32;
+					System.out.println("UPnMAPUP");
 				}
 				else
 				{
-					player.setY(player.getY() + 32);
+					if(player.getY()<levelHeight)
+					{						
+						player.setY(player.getY()+32);
+						playerYMapCoord+=32;
+						System.out.println("TopOfLevel");
+					}
 				}
 			}
-			
 			break;
 		case Input.Keys.S:
-			if(player.getY()-characterEdgeBuffer>0)
+			if (playerYMapCoord - characterEdgeBuffer > 0) 
 			{
+				playerYMapCoord -= 32;
 				player.setY(player.getY() - 32);
-			}
-			else
+				System.out.println("DOWN");
+			} 
+			else 
 			{
-				if (mapy >0) {
+				boolean mapCoord=playerYMapCoord>0;
+				boolean heightbool=maxYCoord-SCREENHEIGHT>0;
+				//playerYMapCoord < camera.viewportHeight && maxYCoord<levelHeight
+				if (mapCoord&&heightbool) 
+				{
 					camera.translate(0, -32);
-					mapy -= 32;
+					playerYMapCoord+=32;
+					maxYCoord-=32;
+					System.out.println("DOWNandMapDown");
 				}
 				else
 				{
-					player.setY(player.getY() - 32);
+					if(player.getY()>0)
+					{
+						player.setY(player.getY()-32);
+						System.out.println("bottomOfMap");
+						playerYMapCoord-=32;
+					}
 				}
 			}
-			
 			break;
+			
+		/*	if (playerYMapCoord > 0) {
+				player.setY(player.getY() - 32);
+				playerYMapCoord -= 32;
+			} else {
+				if (playerYMapCoord == 0) {
+				} else if (playerYMapCoord > 0) {
+					player.setY(player.getY() - 32);
+				} else {
+					camera.translate(0, -32);
+				}
+			}
+
+			break;*/
 		case Input.Keys.A:
 			player.setX(player.getX() - 32);
 			break;
 		case Input.Keys.D:
 			player.setX(player.getX() + 32);
-			break;
-
-		case Input.Keys.LEFT:
-				if(mapx>0)					
-			{
-				camera.translate(-32, 0);
-				mapx -= 32;
-			}
-			break;
-		case Input.Keys.RIGHT:
-			if (mapx + camera.viewportWidth < levelWidth) 
-			{
-				camera.translate(32, 0);
-				mapx += 32;
-			}
-			break;
-		case Input.Keys.UP:
-			if (mapy < camera.viewportHeight) {
-				camera.translate(0, 32);
-				mapy += 32;
-			}
-
-			break;
-		case Input.Keys.DOWN:
-			if (mapy > 0) {
-
-				camera.translate(0, -32);
-				mapy -= 32;
-			}
 			break;
 		case Input.Keys.NUM_1:
 			tiledMap.getLayers().get(0)
@@ -182,7 +251,7 @@ public class Level {
 			System.out.println(Gdx.input.getX() + " "
 					+ (Gdx.graphics.getHeight() - Gdx.input.getY()));
 		case Input.Keys.Z:
-			System.out.println("Mapx:" + mapx + " mapy:" + mapy);
+			System.out.println("playerXMapCoord:" + playerXMapCoord + " playerYMapCoord:" + playerYMapCoord);
 		}
 
 		return false;
